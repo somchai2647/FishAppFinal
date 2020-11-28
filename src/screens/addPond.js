04d12a6c06d0c6c4ae7076fb64077420412cca6d
Somchai <somchai.b@cvc.ac.th>
2020-11-28 21:44:21 +0700

    can post data pond

--- src/screens/addPond.js
+++ src/screens/addPond.js
@@ -3,20 +3,27 @@ import { Text, View, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-na
 import { ScrollView, TextInput } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 import Navbar from "../components/Navbar2";
+import Axios from "../components/API";
 
 export default class addPond extends Component {
+    state = {
+        p_name: '',
+        fish_type: '',
+        p_width: 0.0,
+        p_height: 0.0,
+        p_length: 0.0,
+        p_fish_date: 90,
+        p_number_fish: 100,
+        p_number_success: 0
+    }
     render() {
-        function Apply({ toPage = "", data }) {
-            const navigation = useNavigation();
-            return (
-                <>
-                    <TouchableOpacity style={styles.apply} onPress={() => {
-                        navigation.navigate(toPage, data)
-                    }}>
-                        <Text style={styles.apply_text}>ถัดไป</Text>
-                    </TouchableOpacity>
-                </>
-            )
+        function Apply({ data }) {
+            // Axios.post("/pond/add", data).then(res => {
+            //     console.log(res);
+            // }).catch(err => {
+            //     console.log(err);
+            // });
+            console.log(data)
         }
         return (
             <>
@@ -25,32 +32,40 @@ export default class addPond extends Component {
                     <ScrollView showsVerticalScrollIndicator={false}
                         showsHorizontalScrollIndicator={false}>
                         <View style={styles.input_group}>
-                            <Text style={styles.label}>ชื่อบ่อปลา</Text>
-                            <TextInput style={styles.form_control} ></TextInput>
+                            <Text style={styles.label} >ชื่อบ่อปลา</Text>
+                            <TextInput style={styles.form_control} onChangeText={(p_name) => this.setState({ p_name })} ></TextInput>
                         </View>
                         <View style={styles.input_group}>
                             <Text style={styles.label}>ประเภทปลา</Text>
-                            <TextInput style={styles.form_control} ></TextInput>
+                            <TextInput style={styles.form_control} onChangeText={(fish_type) => this.setState({ fish_type })}></TextInput>
                         </View>
                         <View style={{ marginTop: 20 }}></View>
                         <View style={styles.input_group}>
                             <Text style={styles.label}>ขนาดความกว้าง</Text>
-                            <TextInput style={styles.form_control} keyboardType="decimal-pad"  ></TextInput>
+                            <TextInput style={styles.form_control} keyboardType="decimal-pad" onChangeText={(p_width) => this.setState({ p_width })}></TextInput>
                         </View>
                         <View style={styles.input_group}>
                             <Text style={styles.label}>ขนาดความยาว</Text>
-                            <TextInput style={styles.form_control} keyboardType="decimal-pad"  ></TextInput>
+                            <TextInput style={styles.form_control} keyboardType="decimal-pad" onChangeText={(p_length) => this.setState({ p_length })}></TextInput>
                         </View>
                         <View style={styles.input_group}>
                             <Text style={styles.label}>ขนาดความสูง</Text>
-                            <TextInput style={styles.form_control} keyboardType="decimal-pad"  ></TextInput>
+                            <TextInput style={styles.form_control} keyboardType="decimal-pad" onChangeText={(p_height) => this.setState({ p_height })}></TextInput>
                         </View>
                         <View>
-                            <Apply />
+                            <TouchableOpacity style={styles.apply} onPress={() => {
+                                let data = this.state;
+                                Axios.post("/pond/add", { data }).then(res => {
+                                    console.log(res.data);
+                                }).catch(err => {
+                                    console.log(err);
+                                });
+                            }}>
+                                <Text style={styles.apply_text}>ถัดไป</Text>
+                            </TouchableOpacity>
                         </View>
                     </ScrollView>
                 </SafeAreaView>
-
             </>
 
         )
